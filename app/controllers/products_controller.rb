843fee0caf7c01e5d1b91cc7356b1b04ed4b1835
Jose <josemicrobiologo@gmail.com>
2018-09-10 20:57:56 -0500

    crea update and editviews con extension js.erb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,8 +15,17 @@ class ProductsController < ApplicationController
     @product = Product.create(product_params)
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.update(params[:id], product_params)
+  end
+  
   private
   def product_params
     params.require(:product).permit(:name, :price)
   end
+
 end
