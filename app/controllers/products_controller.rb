3cde5be07211593eb5f880bacde0887d497e6e04
Jose <josemicrobiologo@gmail.com>
2018-09-10 20:32:21 -0500

    agrega los partial form, create, new con extension js.erb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,6 +17,6 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:prodct).permit(:name, :price)
+    params.require(:product).permit(:name, :price)
   end
 end
