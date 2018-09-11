eef1eb71a28ccd7cd0caa0263e99c61305c31b23
Jose <josemicrobiologo@gmail.com>
2018-09-10 19:32:13 -0500

    modifica el archivo products/controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,22 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
 
   def alert_js
     
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+  end
+
+  private
+  def product_params
+    params.require(:prodct).permit(:name, :price)
   end
 end
